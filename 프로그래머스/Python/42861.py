d97f6ed6a86ce9dae16ef658329db0250bbb49bf
yewon918 <yewon918@naver.com>
2022-02-11 17:52:32 +0900

    2/11

---
+++ 프로그래머스/Python/42861.py
@@ -0,0 +1,19 @@
+def solution(n, costs):
+    answer = 0
+    costs.sort(key=lambda x: x[2])  # 비용기준으로 오름차순 정렬
+    connect = set([costs[0][0]])   # 연결을 확인하는 집합
+    #connect = {[costs[0][0]]}
+
+    # Kruskal 알고리즘으로 최소 비용 구하기
+    while len(connect) != n:
+        for cost in costs:
+            if cost[0] in connect and cost[1] in connect:
+                continue
+            if cost[0] in connect or cost[1] in connect:
+                connect.update([cost[0], cost[1]])
+                answer += cost[2]
+                break
+
+    return answer
+
+print(solution(4, [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]))
