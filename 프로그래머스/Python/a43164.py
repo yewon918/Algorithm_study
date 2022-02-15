811539f904c30202dd3cb4943d7de6a8d64aa763
yewon918 <yewon918@naver.com>
2022-02-15 23:56:19 +0900

    2/15

--- 프로그래머스/Python/a43164.py
+++ 프로그래머스/Python/a43164.py
@@ -1,21 +1,27 @@
 def solution(tickets):
-    t = dict()
-    for ticket in tickets:
-        if ticket[0] in t:
-            t[ticket[0]].append(ticket[1])
-        else:
-            t[ticket[0]]=[ticket[1]]
-
-    for k in t.keys():
-        t[k].sort(reverse=True)     # 도착지를 역순으로 넣는다
-
-    st=["ICN"]
     answer = []
+    dest = dict()
 
-    while st:
-        top = st[-1]
-        if top not in t or len(t[top]) == 0:
-            answer.append(st.pop())
+    for ticket in tickets:
+        if ticket[0] in dest:
+            dest[ticket[0]].append(ticket[1])
+        else:
+            dest[ticket[0]] = [ticket[1]]       # []
+
+    for i in dest:
+        dest[i].sort(reverse=True)      # 리스트 역순
+
+    stack = ['ICN']
+
+    while stack:
+        top = stack[-1]
+        if top not in dest or len(dest[top]) == 0:
+            answer.append(top)
+            stack.pop()
+        else:                   # 도착지가 있는 애였을 때
+            stack.append(dest[top][-1])     # 리스트 맨 뒤
+            dest[top].pop()
+
     answer.reverse()
     return answer
 
